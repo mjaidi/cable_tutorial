76dae6338bd333d255abd9eb002e9340c2703b16
Majid Jaidi <majid.jaidi@gmail.com>
2019-08-26 09:40:51 +0100

    Rails New with pg, webpacker and bootstrap with initial setup for Basic messaging app

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :messages
+  NAME_REGEX = /\w+/
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       format: { with: /\A#{NAME_REGEX}\z/i },
+                       length: { maximum: 15 }
+end
